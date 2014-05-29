1d4627ea42b2bdda205d726cce8b2664c983b447
Chinedu Uzoka <acuzoka@gmail.com>
2014-05-29 15:20:48 +0100

    decrease hostadd timeout

--- manifests/hostadd.pp
+++ manifests/hostadd.pp
@@ -13,11 +13,13 @@ define ipa::hostadd (
   $descinfo = rstrip(join(['Added by IPA Puppet module on',$timestamp,$desc], " "))
 
   if $::ipa_adminhomedir and is_numeric($::ipa_adminuidnumber) {
+    unless $host == undef {
     exec { "hostadd-${host}":
       command   => "/sbin/runuser -l admin -c \'/usr/bin/ipa host-add ${host} --force  --locality=\"${locality}\" --location=\"${location}\" --desc=\"${descinfo}\" --platform=\"${clientpf}\" --os=\"${clientos}\" --password=${otp}\'",
       unless    => "/sbin/runuser -l admin -c \'/usr/bin/ipa host-show ${host} >/dev/null 2>&1\'",
-      tries     => '60',
-      try_sleep => '60'
+      tries     => '5',
+      try_sleep => '10'
+    }
     }
   }
 }
