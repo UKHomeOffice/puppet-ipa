af75c3e31d230bbe0ed84501520307bb0d1d4a73
Chinedu Uzoka <acuzoka@gmail.com>
2014-07-22 22:10:37 +0100

    Typo

--- lib/facter/ipa_clientinstall.rb
+++ lib/facter/ipa_clientinstall.rb
@@ -11,7 +11,7 @@ Facter.add("ipa_clientinstall") do
     ENV['TERM'] = ''
     result = "false"
     confine  :osfamily => "RedHat"
-    install = system("/usr/bin/pythond -c 'import os,sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa-client/sysrestore\").has_files() else 1)'>/dev/null 2>&1")
+    install = system("/usr/bin/python -c 'import os,sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa-client/sysrestore\").has_files() else 1)'>/dev/null 2>&1")
     if ! install.nil? and install
       result = "true"
     end
--- lib/facter/ipa_masterinstall.rb
+++ lib/facter/ipa_masterinstall.rb
@@ -14,7 +14,7 @@ Facter.add("ipa_masterinstall") do
     ENV['TERM'] = ''
     result = "false"
     confine  :osfamily => "RedHat"
-    install = system("/usr/bin/pythond -c 'import os,sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa/sysrestore\").has_files() else 1)'>/dev/null 2>&1")
+    install = system("/usr/bin/python -c 'import os,sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa/sysrestore\").has_files() else 1)'>/dev/null 2>&1")
     if ! install.nil? and install
       result = "true"
     end
