f7b155049a6315917590a71a3d0c16854f39a14a
Chinedu Uzoka <acuzoka@gmail.com>
2014-07-22 22:07:25 +0100

    amended facts to correctly report client/master install

--- lib/facter/ipa_clientinstall.rb
+++ lib/facter/ipa_clientinstall.rb
@@ -8,12 +8,14 @@
 
 Facter.add("ipa_clientinstall") do
   setcode do
+    ENV['TERM'] = ''
     result = "false"
     confine  :osfamily => "RedHat"
-    install = Facter::Util::Resolution.exec("/usr/bin/python -c 'import sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa-client/sysrestore > /dev/null 2>&1\").has_files() else 1)' > /dev/null 2>&1")
-    unless install.nil?
-      "true"
+    install = system("/usr/bin/pythond -c 'import os,sys,ipapython.sysrestore; sys.exit(0 if ipapython.sysrestore.FileStore(\"/var/lib/ipa-client/sysrestore\").has_files() else 1)'>/dev/null 2>&1")
+    if ! install.nil? and install
+      result = "true"
     end
-    result 
+    result
   end
 end
+
